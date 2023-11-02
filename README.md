2fb5464fb77f85fad8a56e428b8dcc2a1755d408
Amir Astaneh <amastaneh@gmail.com>
2023-11-02 16:11:19 -0400

    Update name

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# Universal Network Visualization
-Universal Network Visualization Insights presents network data seamlessly, offering clear visuals through charts, graphs, and tables. Dive deep into key metrics across diverse locations.
+# Network Visualization Insights
+Network Visualization Insights presents network data seamlessly, offering clear visuals through charts, graphs, and tables. Dive deep into key metrics across diverse locations.
 
 
--- src/pages/pageSignIn.js
+++ src/pages/pageSignIn.js
@@ -14,8 +14,8 @@ const PageSignIn = () => {
 
     return <div className="flex min-h-full flex-col justify-center px-6 lg:px-8">
         <div className="sm:mx-auto sm:w-full sm:max-w-sm">
-            <img className="mx-auto h-25 w-auto" src="/logo.png" alt="Universal Network Visualization Insights" />
-            <h2 className="text-center text-lg font-bold leading-9 tracking-tight">Universal Network Visualization Insights</h2>
+            <img className="mx-auto h-25 w-auto" src="/logo.png" alt="Network Visualization Insights" />
+            <h2 className="text-center text-lg font-bold leading-9 tracking-tight">Network Visualization Insights</h2>
         </div>
 
         <div className="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
